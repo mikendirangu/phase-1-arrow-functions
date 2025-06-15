ca3e13c5abb00f36d8e64539c353fd1309c7a46c
Big mike <mikendirangu26@gmail.com>
2025-06-15 11:31:00 +0300

    Complete arrow functions for lab

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// Function expression called divide
+const divide = function() {
+  return 2000 / 100;
+};
+
+// Arrow function called square
+const square = (x) => x * x;
+
+// Arrow function called add
+const add = (a, b) => a + b;
+
 // saturdayFun function expression
 const saturdayFun = function(activity = "roller-skate") {
   return `This Saturday, I want to ${activity}!`;
